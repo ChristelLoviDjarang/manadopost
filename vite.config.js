f2bbc489215478173b6853668d80d323adb8c066
ChristelLoviDjarang <christellovi0512@gmail.com>
2025-04-04 16:12:31 +0800

    FIX

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/manadopost",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ['**/*.glb', '**/*.fbx'],
+  assetsInclude: ["**/*.glb", "**/*.fbx"],
   server: {
-    host: true
-  }
-})
+    host: true,
+  },
+});
